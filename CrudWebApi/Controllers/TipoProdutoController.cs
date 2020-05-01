84fa7813e672e716691159a341bbb0d06e0bd9d4
Jhonathan Araujo <JhonathanAraujoBR@Hotmail.com>
2020-05-01 18:32:28 -0300

    Quatro operações básicas testadas com Insomnia

---
+++ CrudWebApi/Controllers/TipoProdutoController.cs
@@ -0,0 +1,88 @@
+﻿using CrudWebApi.DAL;
+using CrudWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace CrudWebApi.Controllers
+{
+    public class TipoProdutoController : ApiController
+    {
+        // Método responsável por listar todos os tipos e seus produtos
+        public IHttpActionResult Get()
+        {
+            return Ok(new TipoProdutoDAO().Listar());
+        }
+
+        // Método responsável por consultar os produtos de um tipo
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                TipoProdutoDAO dao = new TipoProdutoDAO();
+                TipoProduto TipoProduto = dao.Consultar(id);
+
+                // StatusCode 200
+                return Ok(TipoProduto);
+            }
+            // Captura a excessão de chave não encontrada
+            catch (KeyNotFoundException)
+            {
+                // StatusCode 404
+                return NotFound();
+            }
+        }
+
+        // [FromBody]: recebe o conteúdo para ser inserido no corpo da requisição
+        public IHttpActionResult Post([FromBody] TipoProduto TipoProduto)
+        {
+            try
+            {
+                TipoProdutoDAO dao = new TipoProdutoDAO();
+                dao.Inserir(TipoProduto);
+
+                // Cria uma propriedade para efetuar a consulta da informação cadastrada
+                string location = Url.Link("DefaultApi", new { controller = "tipoproduto", id = TipoProduto.IdTipo });
+
+                // StatusCode 201
+                return Created(new Uri(location), TipoProduto);
+            }
+            catch (Exception)
+            {
+                // StatusCode 400
+                return BadRequest();
+            }
+        }
+
+        // Método responsável por deletar um recurso
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                TipoProdutoDAO dao = new TipoProdutoDAO();
+                dao.Excluir(id);
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        // Método responsável por atualizar um recurso
+        public IHttpActionResult Put([FromBody] TipoProduto tipoProduto)
+        {
+            try
+            {
+                TipoProdutoDAO dao = new TipoProdutoDAO();
+                dao.Alterar(tipoProduto);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ CrudWebApi/DAL/TipoProdutoDAO.cs
@@ -0,0 +1,75 @@
+﻿using CrudWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrudWebApi.DAL
+{
+    public class TipoProdutoDAO
+    {
+        // Lista criada para armezenar uma lista de ipo de produto simulando o banco de dados
+        private static Dictionary<long, TipoProduto> bancoTipoProduto = new Dictionary<long, TipoProduto>();
+        private static int contadorBanco = 2;
+
+        // Construtor estático serve para criar objetos do TipoProduto e Produto simulando o banco de dados
+        static TipoProdutoDAO()
+        {
+
+            TipoProduto energiaSolar = new TipoProduto();
+            energiaSolar.IdTipo = 1;
+            energiaSolar.DescricaoTipo = "Energia Solar";
+            energiaSolar.Comercializado = true;
+
+            Produto FotoVoltatica = new Produto();
+            FotoVoltatica.IdProduto = 800;
+            FotoVoltatica.NomeProduto = "Energia Solar Fotovoltatica";
+            FotoVoltatica.Caracteristicas = @"A tecnologia fotovoltaica (FV) 
+                                            converte diretamente os raios 
+                                            solares em eletricidade";
+            FotoVoltatica.PrecoMedio = 4000.00;
+            FotoVoltatica.Logotipo = @"data:image/jpeg;base64";
+            FotoVoltatica.Ativo = true;
+            FotoVoltatica.IdTipoProduto = energiaSolar.IdTipo = 1;
+
+            //Referência do Novo Produto 
+            energiaSolar.Adiciona(FotoVoltatica);
+
+            TipoProduto tinta = new TipoProduto();
+            tinta.IdTipo = 2;
+            tinta.DescricaoTipo = "Tinta";
+            tinta.Comercializado = true;
+
+            //Inserer Registro no Banco
+            bancoTipoProduto.Add(1, energiaSolar);
+            bancoTipoProduto.Add(2, tinta);
+        }
+
+        public void Inserir(TipoProduto tipoProduto)
+        {
+            contadorBanco++;
+            tipoProduto.IdTipo = contadorBanco;
+            bancoTipoProduto.Add(contadorBanco, tipoProduto);
+        }
+
+        public TipoProduto Consultar(int idTipo)
+        {
+            return bancoTipoProduto[idTipo];
+        }
+
+        public IList<TipoProduto> Listar()
+        {
+            return new List<TipoProduto>(bancoTipoProduto.Values);
+        }
+
+        public void Excluir(int idTipo)
+        {
+            bancoTipoProduto.Remove(idTipo);
+        }
+
+        public void Alterar(TipoProduto tipoProduto)
+        {
+            bancoTipoProduto[tipoProduto.IdTipo] = tipoProduto;
+        }
+    }
+}
